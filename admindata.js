6adacd2fef322bc0486be51e2904f3b1dc86186b
minjun22 <109238046+minjun22@users.noreply.github.com>
2024-10-30 19:15:54 +0900

    Create admindata.js

---
+++ admindata.js
@@ -0,0 +1,27 @@
+// admindata.js
+export const adminData = [
+    { x: -400, z: 100, name: '민석시', type: 'city' },
+    { x: -1800, z: 1000, name: '현준시', type: 'city' },
+    { x: -580, z: -150, name: '엉재군', type: 'district' },
+    { x: -1612, z: 1340, name: '현석군', type: 'district' },
+    { x: -1000, z: 1300, name: '나혁군', type: 'district' },
+    { x: 700, z: -100, name: '민규군', type: 'district' },
+    { x: -1400, z: 1000, name: '정준군', type: 'district' },
+    { x: -2400, z: 600, name: '슬기군', type: 'district' },
+    { x: -2724, z: 99, name: '경민군', type: 'district' },
+    { x: -365, z: 844, name: '이경동', type: 'district' },
+    { x: -1000, z: 500, name: '동해군', type: 'district' },
+    { x: -160, z: -80, name: '중구', type: 'district' },
+    { x: -561, z: 1194, name: '민준구', type: 'district' },
+    { x: 300, z: 690, name: '남동구', type: 'district' },
+    { x: -294, z: 185, name: '강남동', type: 'neighborhood' },
+    { x: -500, z: -400, name: '공항동', type: 'neighborhood' },
+    { x: -1200, z: 0, name: '송도동', type: 'neighborhood' },
+    { x: 550, z: 770, name: '양촌동', type: 'neighborhood' },
+    { x: 1000, z: 820, name: '동막동', type: 'neighborhood' },
+    { x: -2200, z: 870, name: '화곡동', type: 'neighborhood' },
+    { x: -2800, z: 1050, name: '평동', type: 'neighborhood' },
+    { x: 222, z: 1277, name: '한반도동', type: 'neighborhood' },
+    { x: -1200, z: 1250, name: '회기리', type: 'neighborhood' },
+    // 추가 데이터
+];
