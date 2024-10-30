b6cd5c2d1972c236ef0c998317670515eaac10f4
minjun22 <109238046+minjun22@users.noreply.github.com>
2024-10-30 19:15:30 +0900

    Create building.js

---
+++ building.js
@@ -0,0 +1,334 @@
+export const buildingData = [
+    {
+        name: '맥도날드',
+        coordinates: [-537, 1337], // 마인크래프트 좌표
+        zoomLevels: [7, 8], // 표시할 줌 레벨
+        type: 'hamburger' // 업종
+    },
+    {
+        name: '민석상공회의소',
+        coordinates: [-457, 1309],
+        zoomLevels: [7, 8],
+        type: 'office' // 업종
+    },
+    {
+        name: '경찰청',
+        coordinates: [-444, 1278],
+        zoomLevels: [7, 8],
+        type: 'office' // 업종
+    },
+    {
+        name: 'LH행복주택',
+        coordinates: [-605, 1377],
+        zoomLevels: [7, 8],
+        type: 'apt' // 업종
+    },
+    {
+        name: '민준고등학교',
+        coordinates: [-616, 1299],
+        zoomLevels: [5, 6, 7, 8],
+        type: 'school' // 업종
+    },
+    {
+        name: '민준구-공영주차장',
+        coordinates: [-459, 1371],
+        zoomLevels: [7, 8],
+        type: 'parking' // 업종
+    },
+    {
+        name: '',
+        coordinates: [-547, 1484],
+        zoomLevels: [8],
+        type: 'parking' // 업종
+    },
+    {
+        name: '서민준역점',
+        coordinates: [-547, 1469],
+        zoomLevels: [5, 6, 7, 8],
+        type: 'starbucks' // 업종
+    },
+    {
+        name: '오리엔탈호텔',
+        coordinates: [-474, 1238],
+        zoomLevels: [5, 6, 7, 8],
+        type: 'default' // 업종
+    },
+ {
+        name: '다운전자',
+        coordinates: [-434, 1238],
+        zoomLevels: [8],
+        type: 'office' // 업종
+    },
+ {
+        name: '한국바이오협회',
+        coordinates: [-430, 1340],
+        zoomLevels: [5,6,7,8],
+        type: 'office' // 업종
+    },
+ {
+        name: 'BK생각하는수학원',
+        coordinates: [-529, 1275],
+        zoomLevels: [8],
+        type: 'default' // 업종
+    },
+ {
+        name: '부산저축은행',
+        coordinates: [-357, 1346],
+        zoomLevels: [7, 8],
+        type: 'office' // 업종
+    },
+ {
+        name: '민준역점',
+        coordinates: [-359, 1245],
+        zoomLevels: [7, 8],
+        type: 'busanbank' // 업종
+    },
+ {
+        name: '',
+        coordinates: [-1200, 1460],
+        zoomLevels: [7, 8],
+        type: 'parking' // 업종
+    },
+ {
+        name: '남회기역점',
+        coordinates: [-1179, 1440],
+        zoomLevels: [6, 7, 8],
+        type: 'gs25' // 업종
+    },
+ {
+        name: '대통령의 집',
+        coordinates: [-1182, 1393],
+        zoomLevels: [7, 8],
+        type: 'default' // 업종
+    },
+ {
+        name: '남회기-터미널',
+        coordinates: [-1140, 1458],
+        zoomLevels: [8],
+        type: 'default' // 업종
+    },
+ {
+        name: '임현석의 집',
+        coordinates: [-1330, 1247],
+        zoomLevels: [5,6,7,8],
+        type: 'apt' // 업종
+    },
+ {
+        name: '회기비석',
+        coordinates: [-1236, 1438],
+        zoomLevels: [7,8],
+        type: 'default' // 업종
+    },
+ {
+        name: '천주교-토현성당',
+        coordinates: [-1490, 1225],
+        zoomLevels: [5,6,7,8],
+        type: 'church' // 업종
+    },
+ {
+        name: '현준시청',
+        coordinates: [-1865, 997],
+        zoomLevels: [7],
+        type: 'default' // 업종
+    },
+ {
+        name: '현준시청',
+        coordinates: [-1840, 997],
+        zoomLevels: [8],
+        type: 'default' // 업종
+    },
+ {
+        name: '현준시의회',
+        coordinates: [-1885, 997],
+        zoomLevels: [8],
+        type: 'default' // 업종
+    },
+ {
+        name: '화곡동-주민센터',
+        coordinates: [-2126, 874],
+        zoomLevels: [6,7,8],
+        type: 'default' // 업종
+    },
+ {
+        name: '녹차R&D센터',
+        coordinates: [-2090, 876],
+        zoomLevels: [8],
+        type: 'office' // 업종
+    },
+ {
+        name: '현준교도소',
+        coordinates: [-2560, 682],
+        zoomLevels: [5,6,7,8],
+        type: 'default' // 업종
+    },
+ {
+        name: '일본정원',
+        coordinates: [152, 1150],
+        zoomLevels: [7,8],
+        type: 'park' // 업종
+    },
+ {
+        name: '평의 집',
+        coordinates: [681, 906],
+        zoomLevels: [7,8],
+        type: 'apt' // 업종
+    },
+ {
+        name: '민준의 집',
+        coordinates: [709, 898],
+        zoomLevels: [8],
+        type: 'apt' // 업종
+    },
+ {
+        name: '양촌타워',
+        coordinates: [731, 982],
+        zoomLevels: [5,6,7,8],
+        type: 'default' // 업종
+    },
+ {
+        name: '꼬꼬팜',
+        coordinates: [696, 795],
+        zoomLevels: [7,8],
+        type: 'default' // 업종
+    },
+ {
+        name: '동막시장',
+        coordinates: [868, 779],
+        zoomLevels: [5,6,7,8],
+        type: 'default' // 업종
+    },
+ {
+        name: '국립동막도서관',
+        coordinates: [848, 696],
+        zoomLevels: [7,8],
+        type: 'default' // 업종
+    },
+ {
+        name: '양촌동-행정복지센터',
+        coordinates: [735, 682],
+        zoomLevels: [6,7,8],
+        type: 'default' // 업종
+    },
+ {
+        name: '녹차교도소',
+        coordinates: [813, 515],
+        zoomLevels: [6,7,8],
+        type: 'default' // 업종
+    },
+ {
+        name: '장지혁별장',
+        coordinates: [764, 397],
+        zoomLevels: [7,8],
+        type: 'default' // 업종
+    },
+ {
+        name: '양촌산휴식터',
+        coordinates: [746, 895],
+        zoomLevels: [6,7,8],
+        type: 'park' // 업종
+    },
+ {
+        name: '카마신 신전',
+        coordinates: [-2452, 44],
+        zoomLevels: [5,6,7,8],
+        type: 'church' // 업종
+    },
+ {
+        name: '민규교통공사 본사',
+        coordinates: [-170, 128],
+        zoomLevels: [6,7,8],
+        type: 'office' // 업종
+    },
+ {
+        name: '국회의사당',
+        coordinates: [-170, 171],
+        zoomLevels: [8],
+        type: 'default' // 업종
+    },
+ {
+        name: '한국철도공사-본사',
+        coordinates: [-159, -7],
+        zoomLevels: [6,7,8],
+        type: 'office' // 업종
+    },
+ {
+        name: '강남동-행정복지센터',
+        coordinates: [-112, 266],
+        zoomLevels: [7,8],
+        type: 'default' // 업종
+    },
+ {
+        name: '서강동-행정복지센터',
+        coordinates: [-124, -6],
+        zoomLevels: [8],
+        type: 'default' // 업종
+    },
+ {
+        name: '현준시청 소공원',
+        coordinates: [-1865, 970],
+        zoomLevels: [8],
+        type: 'park' // 업종
+    },
+ {
+        name: '구 현준역-문화공원',
+        coordinates: [-1830, 888],
+        zoomLevels: [6,7,8],
+        type: 'park' // 업종
+    },
+ {
+        name: '현준 협곡코스터',
+        coordinates: [-1770, 761],
+        zoomLevels: [6,7,8],
+        type: 'park' // 업종
+    },
+ {
+        name: '스토리웨이-강남점',
+        coordinates: [-238, 356],
+        zoomLevels: [7,8],
+        type: 'default' // 업종
+    },
+ {
+        name: '스토리웨이 민석점',
+        coordinates: [-268, -195],
+        zoomLevels: [6,7,8],
+        type: 'default' // 업종
+    },
+ {
+        name: '민석시의회',
+        coordinates: [-104, -114],
+        zoomLevels: [7,8],
+        type: 'default' // 업종
+    },
+ {
+        name: '민준의 집-(부지)',
+        coordinates: [-202, -180],
+        zoomLevels: [7,8],
+        type: 'apt' // 업종
+    },
+ {
+        name: '중구청 공원',
+        coordinates: [-164, -113],
+        zoomLevels: [8],
+        type: 'park' // 업종
+    },
+ {
+        name: '엉재동-행정복지센터',
+        coordinates: [-417, -12],
+        zoomLevels: [6,7,8],
+        type: 'default' // 업종
+    },
+ {
+        name: '엉재농업공원',
+        coordinates: [-616, 38],
+        zoomLevels: [6,7,8],
+        type: 'park' // 업종
+    },
+ {
+        name: '국회의사당(송도)',
+        coordinates: [-1240, -40],
+        zoomLevels: [5,6,7,8],
+        type: 'default' // 업종
+    },
+
+    // 다른 건물들 추가
+];
