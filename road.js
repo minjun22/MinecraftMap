c89f1acdd81c2045c9a43f5cd25eb97c7ee69256
minjun22 <109238046+minjun22@users.noreply.github.com>
2024-10-30 19:16:47 +0900

    Create road.js

---
+++ road.js
@@ -0,0 +1,320 @@
+
+export const roadData = [
+    {
+        type: 'highway',
+        name: '',
+        coordinates: [
+            [-280, 778],
+            [-878, 777],
+            [-955, 785],
+            [-1676, 785],
+        ],
+        zoomLevels: [5, 6, 7, 8]
+    },
+    {
+        type: 'highway',
+        name: '현준민석고속도로',
+        coordinates: [
+            [-280, 788],
+            [-347, 788],
+            [-425, 794],
+            [-1460, 794],
+            [-1515, 803],
+            [-1676, 803],
+        ],
+        zoomLevels: [5, 6, 7, 8]
+    },
+    {
+        type: 'highway',
+        name: '',
+        coordinates: [
+            [-1499, 785],
+            [-1508, 778],
+            [-1531, 778],
+            [-1558, 773],
+            [-1592, 740],
+            [-1611, 740],
+            [-1629, 758],
+            [-1629, 897],
+        ],
+        zoomLevels: [6, 7, 8]
+    },
+    {
+        type: 'highway',
+        name: '',
+        coordinates: [
+            [-1615, 897],
+            [-1615, 844],
+            [-1581, 810],
+            [-1554, 810],
+            [-1544, 803],
+        ],
+        zoomLevels: [6, 7, 8]
+    },
+    {
+        name: '민석대로',
+        type: 'mainRoad',
+        coordinates: [
+            [-60, -42],
+            [-700, -42],
+            [-725, -17],
+            [-725, 197],
+            [-700, 222],
+        ],
+        zoomLevels: [5, 6, 7, 8]
+    },
+    {
+        name: '강남대로',
+        type: 'gukdo',
+        coordinates: [
+            [-54, 222],
+            [-700, 222]
+        ],
+        zoomLevels: [5, 6, 7, 8]
+    },
+    {
+        name: '서강로',
+        type: 'road',
+        coordinates: [
+            [-204, -151],
+            [-204, 222]
+        ],
+        zoomLevels: [6, 7, 8]
+    },
+    {
+        name: '엉재대로',
+        type: 'gukdo',
+        coordinates: [
+            [-59, -151],
+            [-1186, -151]
+        ],
+        zoomLevels: [5, 6, 7, 8]
+    },
+    {
+        name: '현준민석고속도로(2026년 예정)',
+        type: 'unopened',
+        coordinates: [
+            [-1676, 803],
+            [-2382, 803],
+            [-2429, 799],
+            [-2749, 799]
+        ],
+        zoomLevels: [5, 6, 7, 8]
+    },
+    {
+        name: '',
+        type: 'unopened',
+        coordinates: [
+            [-1676, 785],
+            [-2102, 785],
+            [-2120, 787],
+            [-2386, 787],
+            [-2445, 790],
+            [-2749, 790]
+        ],
+        zoomLevels: [5, 6, 7, 8]
+    },
+    {
+        name: '',
+        type: 'street',
+        coordinates: [
+            [-132, -151],
+            [-132, -42],
+        ],
+        zoomLevels: [7, 8]
+    },
+    {
+        name: '',
+        type: 'street',
+        coordinates: [
+            [-132, -94],
+            [-94, -94],
+            [-94, -64],
+        ],
+        zoomLevels: [7, 8]
+    },
+   {
+        name: '이경로',
+        type: 'road',
+        coordinates: [
+            [-280, 223],
+            [-280, 846],
+        ],
+        zoomLevels: [5, 6, 7, 8]
+    },
+   {
+        name: '직삼대로',
+        type: 'gukdo',
+        coordinates: [
+            [-280, 402],
+            [197, 402],
+        ],
+        zoomLevels: [5, 6, 7, 8]
+    },
+   {
+        name: '강남역로',
+        type: 'road',
+        coordinates: [
+            [-75, 222],
+            [-75, 402],
+        ],
+        zoomLevels: [5, 6, 7, 8]
+    },
+    {
+        name: '동해1길',
+        type: 'street',
+        coordinates: [
+            [-998, 585],
+            [-910, 585],
+        ],
+        zoomLevels: [7, 8]
+    },
+    {
+        name: '남산대로',
+        type: 'gukdo',
+        coordinates: [
+            [-280, 647],
+            [1285, 647],
+        ],
+        zoomLevels: [5, 6, 7, 8]
+    },
+    {
+        name: '남산대로98번길',
+        type: 'road',
+        coordinates: [
+            [712, 647],
+            [712, 809],
+            [424, 809],
+        ],
+        zoomLevels: [6, 7, 8]
+    },
+    {
+        name: '양촌로',
+        type: 'mainRoad',
+        coordinates: [
+            [642, 809],
+            [642, 1007],
+        ],
+        zoomLevels: [6, 7, 8]
+    },
+    {
+        name: '',
+        type: 'street',
+        coordinates: [
+            [642, 886],
+            [730, 886],
+        ],
+        zoomLevels: [7, 8]
+    },
+    {
+        name: '',
+        type: 'street',
+        coordinates: [
+            [691, 886],
+            [691, 952],
+            [715, 952],
+            [715, 1007],
+        ],
+        zoomLevels: [7, 8]
+    },
+    {
+        name: '경촌로',
+        type: 'mainRoad',
+        coordinates: [
+            [424, 1007],
+            [993, 1007],
+        ],
+        zoomLevels: [6, 7, 8]
+    },
+    {
+        name: '동막시장로',
+        type: 'mainRoad',
+        coordinates: [
+            [905, 647],
+            [905, 1007],
+        ],
+        zoomLevels: [6, 7, 8]
+    },
+    {
+        name: '양촌서로',
+        type: 'mainRoad',
+        coordinates: [
+            [424, 647],
+            [424, 1007],
+        ],
+        zoomLevels: [6, 7, 8]
+    },
+    {
+        name: '',
+        type: 'street',
+        coordinates: [
+            [-394, 1255],
+            [-641, 1255],
+        ],
+        zoomLevels: [7, 8]
+    },
+    {
+        name: '',
+        type: 'street',
+        coordinates: [
+            [-394, 1355],
+            [-448, 1355],
+            [-448, 1331],
+            [-506, 1331],
+        ],
+        zoomLevels: [7, 8]
+    },
+    {
+        name: '',
+        type: 'street',
+        coordinates: [
+            [-506, 1360],
+            [-641, 1360],
+        ],
+        zoomLevels: [7, 8]
+    },
+    {
+        name: '',
+        type: 'street',
+        coordinates: [
+            [-556, 1440],
+            [-556, 1489],
+            [-550, 1494],
+            [-507, 1494],
+        ],
+        zoomLevels: [7, 8]
+    },
+   {
+        name: '민준중앙로',
+        type: 'road',
+        coordinates: [
+            [-506, 1202],
+            [-506, 1579],
+        ],
+        zoomLevels: [6, 7, 8]
+    },
+   {
+        name: '서민준로',
+        type: 'road',
+        coordinates: [
+            [-641, 1202],
+            [-641, 1440],
+        ],
+        zoomLevels: [6, 7, 8]
+    },
+   {
+        name: '민준로',
+        type: 'mainRoad',
+        coordinates: [
+            [-688, 1202],
+            [-405, 1202],
+            [-398, 1206],
+            [-394, 1214],
+            [-394, 1427],
+            [-397, 1435],
+            [-406, 1440],
+            [-668, 1440],
+        ],
+        zoomLevels: [5, 6, 7, 8]
+    },
+];
